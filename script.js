203014d251e8caf19e7712b01d66ec33e395082d
ckneace <35378567+ckneace@users.noreply.github.com>
2018-03-17 21:31:16 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,72 +1,110 @@
 var app = new Vue({
-          el: '#app',
-          data: {
-          name: '',
-          number: '',
-          current: '',
-          jerseyNumber: '',
-          nationality: '',
-          // max: '',
-          // current: {},
-          // loading: true,
-          // addedComment: '',
-          // addedTime:'',
-          // comments: {},
+  el: '#app',
+  data: {
+    items: [],
+    text: 'First Name',
+    text2: 'Last Name',
+    show: 'all',
+    drag: {},
+  },
+  computed: {
+    activeItems: function() {
+      return this.items.filter(function(item) {
+	return !item.completed;
+      });
+    },
+    filteredItems: function() {
+      if (this.show === 'active')
+	return this.items.filter(function(item) {
+	  return !item.completed;
+	});
+      if (this.show === 'completed')
+	return this.items.filter(function(item) {
+	  return item.completed;
+	});
+      return this.items;
+    },
+  },
+  methods: {
+    created: function() {
+      this.getItems();
+    },
 
-          },
-          created: function() {
-          this.xkcd();
-          },
-          methods: {
-          xkcd: function() {
-            console.log("hello");
-          // this.loading = true;
-          fetch('http://api.football-data.org/v1/teams/66/players', {
-            headers: {'X-Auth-Token': 'f2e3e4a8a7544e8fa5316b88a0c47f80'}}).then(response => {
-          return response.json();
-        }).then(json => {  console.log("hello2");
-          this.current = json;
-          // this.name = json.players[0].name;
-           // var regex = /.*?(\d+)$/; // the ? makes the first part non-greedy
-           // var res = regex.exec(json.fixtures[0]._links.awayTeam.href);
-           // var teamId = res[1];
-           console.log(json);
-
-         return true;
-        }).catch(err => {
-
-        });
-          },
-
-          getRandom: function(min, max) {
-                  min = Math.ceil(min);
-                  max = Math.floor(max);
-                  return Math.floor((Math.random() * max) + 1);//The maximum and minimum are inclusive
-                  },
-          randomPage: function() {
-            console.log("here");
-            console.log(this.current.players);
-            this.number = this.getRandom(1, this.current.players.length);
-            this.name = "Name: " + this.current.players[this.number].name;
-            this.jerseyNumber = "Jersey Number: " + this.current.players[this.number].jerseyNumber;
-            this.nationality = "Nationality: " + this.current.players[this.number].nationality;
-                  },
-
-
-
-            },
-                  computed: {
-
-
-                  },
-                  watch: {
-                  number: function(value,oldvalue) {
-                  if (oldvalue === '') {
-                  this.max = value;
-                  } else {
-                  this.xkcd();
-                  }
-                  },
-                  },
-
-              });
+    getItems: function() {
+      axios.get("/api/items").then(response => {
+        this.items = response.data;
+        return true;
+      }).catch(err => {
+      });
+    },
+    addItem: function() {
+      axios.post("/api/items", {
+	       text: "First Number: " + this.text,
+         text2: "Last Name:" + this.text2,
+	        completed: false,
+        }).then(response => {
+	this.text = "";
+  this.text2 = "";
+	this.getItems();
+	return true;
+      }).catch(err => {
+      });
+    },
+    completeItem: function(item) {
+      axios.put("/api/items/" + item.id, {
+	text: item.text,
+  text2: item.text2,
+	completed: !item.completed,
+	orderChange: false,
+      }).then(response => {
+	return true;
+      }).catch(err => {
+      });
+    },
+    deleteItem: function(item) {
+      axios.delete("/api/items/" + item.id).then(response => {
+	this.getItems();
+	return true;
+      }).catch(err => {
+      });
+    },
+    showAll: function() {
+      this.show = 'all';
+    },
+    showActive: function() {
+      this.show = 'active';
+    },
+    showCompleted: function() {
+      this.show = 'completed';
+    },
+    deleteCompleted: function() {
+      this.items.forEach(item => {
+	if (item.completed)
+	  this.deleteItem(item)
+      });
+    },
+    sort: function(){
+      axios.put("/api/sort/").then(response => {
+        this.getItems();
+        return true;
+      }).catch(err => {
+      });
+    },
+    dragItem: function(item) {
+      this.drag = item;
+    },
+    dropItem: function(item) {
+      axios.put("/api/items/" + this.drag.id, {
+	text: this.drag.text,
+	text2: this.drag.text2,
+	completed: this.drag.completed,
+	orderChange: true,
+	orderTarget: item.id
+      }).then(response => {
+	this.getItems();
+	return true;
+      }).catch(err => {
+      });
+    },
+  }
+});
